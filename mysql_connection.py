284c001daed969e5d5f714559ce8fda9e7fd62e4
Alejandro Morrison <nelsonmorrisonhidalgo0@gmail.com>
2018-03-12 18:05:22 -0400

    cambios nuevos

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,17 +1,31 @@
 import mysql.connector
 
-config = {
-    'user': 'root',
-    'host': '127.0.0.1',
-    'database': 'PaisPosiblePython'
-}
 
+config = {'user': 'root', 'host': '127.0.0.1','database': 'prueba'}
 cnx = mysql.connector.connect(**config)
-
-DB_NAME = 'PaisPosiblePython'
 cursor = cnx.cursor()
+
+#DB_NAME = 'PaisPosiblePython'
+
+NombreTabla = "Prueba"
+NombreTabla1 = 5
+NombreTabla2 = 98
+#comando para crear tabla y 
+#comandoCrearTabla = "CREATE TABLE {} ({} VARCHAR(20), {} VARCHAR(20))".format(NombreTabla, NombreTabla1, NombreTabla2)
+comandoInsertarDatosTabla = "INSERT INTO {} VALUES ({}, {})".format(NombreTabla1, NombreTabla1, NombreTabla2)
+#comandoSeleccionarDatosTabla = "SELECT"
+#INSERT INTO Prueba VALUES(1, 2)
+#print(comando)
 #crear tabla
-cursor.execute("CREATE DATABASE PaisPosible_EncuestasHoy")
-cursor.execute("")
+#CREATEDATABASEMYSQL = cursor.execute("CREATE DATABASE PaisPosible_EncuestasHoy")
+#SELECTDATABASE = cursor.execute("SELECT * FROM hola")
+#creartablepython = cursor.execute(comandoCrearTabla)
+InsertarPython = cursor.execute(comandoInsertarDatosTabla)
+#CREATETABLEMYSQL
+
 cursor.close()
 cnx.close()
+
+        
+
+
