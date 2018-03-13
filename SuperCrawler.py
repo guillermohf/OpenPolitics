4a5fda7d88a8fe1309431de9e7ac73ac18252fb6
Alejandro Morrison <nelsonmorrisonhidalgo0@gmail.com>
2018-03-13 14:02:57 -0400

    mejorando estructura en modulos

--- SuperCrawler.py
+++ SuperCrawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import __urls
 
 class CrawlerRefinadoOOP:
     def __init__(self,AskURL, url, Pages, Titulo, Texto, InformacionTitulo, InformacionTexto):
@@ -32,4 +33,22 @@ class CrawlerRefinadoOOP:
             for div1 in RespuestasEncuestas:
                 return Respuestas2.append("{}".format(div1.text))
                 return Respuestas2
+    def PeriodicoHoy():
+        pages = []
+        for i in range(1, 105):
+            url = 'http://hoy.com.do/encuestas/' + "?poll_page=" + str(i)
+            pages.append(url)
+        __Titulo = []
+        __texto = []
+        url = "hoy.com.do/encuestas"
+        r = requests.get(pages)
+        soup = r.content
+        TITULO = soup.select(".wp-polls-ans")
+        TEXTOS = soup.findAll("p", {"class": "titleOnePollGBH"})
+        for __items in TITULO:
+            return __titulo.append("{}".format(__items.text))
+            print(__titulo)
+
+CR = CrawlerRefinadoOOP.PeriodicoHoy()
+print(CR)
 
---
+++ __urls.py
@@ -0,0 +1,17 @@
+import periodicoslink
+#Periodicos organizados segun una lista, para llamar al crawler
+Hoy = "http://hoy.com.do/encuestas/"
+Listin_Diario = "https://www.listindiario.com/encuestas"
+La_Informacion = "http://www.lainformacion.com.do/modulos/encuestas/encuestas_anteriores.php?idEnc=744"
+La_bazuca = "http://www.labazuca.com/encuestas/"
+El_Nacional  = "http://elnacional.com.do/encuestas/"
+
+
+
+Sitios_web = [
+                Hoy,
+                Listin_Diario,
+                La_Informacion,
+                La_bazuca,
+                El_Nacional,
+            ]
